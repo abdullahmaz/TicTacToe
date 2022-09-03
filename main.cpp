5713e96afcd5d94c268f68a70d2173fbd0e870dd
Abdullah <abdullahmaz2003@gmail.com>
2022-09-03 18:36:21 +0500

    In 2-D array and check's in loops

--- main.cpp
+++ main.cpp
@@ -1,40 +1,54 @@
 #include <iostream>
 using namespace std;
 
-char grid[9]={'1','2','3','4','5','6','7','8','9'};
+char grid[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 int check(){
-    if(grid[0]=='x' && grid[1]=='x' && grid[2]=='x' || grid[3]=='x' && grid[4]=='x' && grid[5]=='x' || grid[6]=='x' && grid[7]=='x' && grid[8]=='x' || 
-       grid[0]=='x' && grid[3]=='x' && grid[6]=='x' || grid[1]=='x' && grid[4]=='x' && grid[7]=='x' ||grid[2]=='x' && grid[5]=='x' && grid[8]=='x' ||
-       grid[0]=='x' && grid[4]=='x' && grid[8]=='x' || grid [2]=='x' && grid[4]=='x' && grid[6]=='x'){
+    
+    for(int i=0; i<=2; i++){
+        for(int j=0; j<=2; j++){
+            if(grid[i][j]=='x' && grid[i+1][j]=='x' && grid[i+2][j]=='x'){
+            cout<<"Player 1 wins !!\n";
+            return 1;        
+            }
+            if(grid[i][j]=='o' && grid[i+1][j]=='o' && grid[i+2][j]=='o'){
+            cout<<"Player 2 wins !!\n";
+            return 1;
+            }
+            if(grid[i][j]=='x' && grid[i][j+1]=='x' && grid[i][j+2]=='x'){
+            cout<<"Player 1 wins !!\n";
+            return 1;        
+            }
+            if(grid[i][j]=='o' && grid[i][j+1]=='o' && grid[i][j+2]=='o'){
+            cout<<"Player 2 wins !!\n";
+            return 1;
+            }
+        }
+    }
+    if(grid[0][0]=='x' && grid[1][1]=='x' && grid[2][2]=='x' || grid[0][2]=='x' && grid[1][1]=='x' && grid[2][0]=='x'){
         cout<<"Player 1 wins !!\n";
         return 1;
     }
-
-    if(grid[0]=='o' && grid[1]=='o' && grid[2]=='o' || grid[3]=='o' && grid[4]=='o' && grid[5]=='o' || grid[6]=='o' && grid[7]=='o' && grid[8]=='o' || 
-       grid[0]=='o' && grid[3]=='o' && grid[6]=='o' || grid[1]=='o' && grid[4]=='o' && grid[7]=='o' ||grid[2]=='o' && grid[5]=='o' && grid[8]=='o' ||
-       grid[0]=='o' && grid[4]=='o' && grid[8]=='o' || grid[2]=='o' && grid[4]=='o' && grid[6]=='o'){
-        cout<<"Player 2 wins !!\n";    
+    if(grid[0][0]=='o' && grid[1][1]=='o' && grid[2][2]=='o' || grid[0][2]=='o' && grid[1][1]=='o' && grid[2][0]=='o'){
+        cout<<"Player 2 wins !!\n";
         return 1;
     }
 }
 void my_grid(){
     
-    int x=0;
     system("clear");
     for(int i=0; i<=2; i++){
             for(int j=0; j<=2; j++){
-                cout<<grid[x]<<"  ";
-                x++;
-            }
+                cout<<grid[i][j]<<"  ";
+                }
         cout<<"\n"; 
     }
 }
-
 int main(){
 
-    int turn,player;
-    char symbol;
+    int player;
+    char turn,symbol;
     my_grid();
+
 
     for(int a=0; a<=8; a++){
         if(a%2 == 0){
@@ -46,13 +60,17 @@ int main(){
         }
         cout<<"Player "<<player<<"'s Turn: ";
         cin>>turn;
-        grid[turn-1]=symbol;
+        for(int i=0; i<=2; i++){
+            for(int j=0; j<=2; j++){
+                if(grid[i][j] == turn)
+                grid[i][j] = symbol;
+            } 
+        }
         my_grid();
         if(check()==1)
         break;
         if(a==8)
         cout<<"It's a tie !!\n";
     }
-    
     return 0;
 }
