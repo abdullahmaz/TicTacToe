e565dcf82081eb232d7f5958c4391a1261718140
Abdullah Mazhar <112191836+abdullahmaz@users.noreply.github.com>
2024-03-26 20:41:54 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,40 +2,45 @@
 using namespace std;
 
 char grid[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-int check(){
-    
-    for(int i=0; i<=2; i++){
-        for(int j=0; j<=0; j++){
-            if(grid[i][j]=='x' && grid[i+1][j]=='x' && grid[i+2][j]=='x'){
-            cout<<"Player 1 wins !!\n";
-            return 1;        
-            }
-            if(grid[i][j]=='o' && grid[i+1][j]=='o' && grid[i+2][j]=='o'){
-            cout<<"Player 2 wins !!\n";
+int check() {
+    // Check rows and columns
+    for (int i = 0; i < 3; i++) {
+        if (grid[i][0] == 'x' && grid[i][1] == 'x' && grid[i][2] == 'x') {
+            cout << "Player 1 wins !!\n";
             return 1;
-            }
-            if(grid[i][j]=='x' && grid[i][j+1]=='x' && grid[i][j+2]=='x'){
-            cout<<"Player 1 wins !!\n";
-            return 1;        
-            }
-            if(grid[i][j]=='o' && grid[i][j+1]=='o' && grid[i][j+2]=='o'){
-            cout<<"Player 2 wins !!\n";
+        }
+        if (grid[i][0] == 'o' && grid[i][1] == 'o' && grid[i][2] == 'o') {
+            cout << "Player 2 wins !!\n";
             return 1;
-            }
+        }
+        if (grid[0][i] == 'x' && grid[1][i] == 'x' && grid[2][i] == 'x') {
+            cout << "Player 1 wins !!\n";
+            return 1;
+        }
+        if (grid[0][i] == 'o' && grid[1][i] == 'o' && grid[2][i] == 'o') {
+            cout << "Player 2 wins !!\n";
+            return 1;
         }
     }
-    if(grid[0][0]=='x' && grid[1][1]=='x' && grid[2][2]=='x' || grid[0][2]=='x' && grid[1][1]=='x' && grid[2][0]=='x'){
-        cout<<"Player 1 wins !!\n";
+
+    // Check diagonals
+    if (grid[0][0] == 'x' && grid[1][1] == 'x' && grid[2][2] == 'x' ||
+        grid[0][2] == 'x' && grid[1][1] == 'x' && grid[2][0] == 'x') {
+        cout << "Player 1 wins !!\n";
         return 1;
     }
-    if(grid[0][0]=='o' && grid[1][1]=='o' && grid[2][2]=='o' || grid[0][2]=='o' && grid[1][1]=='o' && grid[2][0]=='o'){
-        cout<<"Player 2 wins !!\n";
+    if (grid[0][0] == 'o' && grid[1][1] == 'o' && grid[2][2] == 'o' ||
+        grid[0][2] == 'o' && grid[1][1] == 'o' && grid[2][0] == 'o') {
+        cout << "Player 2 wins !!\n";
         return 1;
     }
+
+    // No winner yet
+    return 0;
 }
 void my_grid(){
     
-    system("clear");
+    system("cls");
     for(int i=0; i<=2; i++){
             for(int j=0; j<=2; j++){
                 cout<<grid[i][j]<<"  ";
